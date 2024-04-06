f7d1d232706c94e8f81b18cf85833ecb0f9037bf
Umar <b509615@my.leedscitycollege.ac.uk>
2024-04-06 16:32:52 +0100

    working nav bar, base project for fragment development

---
+++ app/src/main/java/com/example/digitalentrepreneurshipapp/MainActivity.java
@@ -0,0 +1,72 @@
+package com.example.digitalentrepreneurshipapp;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private FrameLayout frameLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        frameLayout = findViewById(R.id.frameLayout);
+
+        BottomNavigationView mBottomNavigationView = (BottomNavigationView)findViewById(R.id.bottomNavigationView);
+        mBottomNavigationView.getMenu().findItem(R.id.navworkouts).setChecked(true); //load navigation bar default when first opened app
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.navnutrition) {
+                    loadFragment(new NutritionFragment(), false);
+
+                } else if (itemId == R.id.navcalendar) {
+                    loadFragment(new CalendarFragment(), false);
+
+                } else if (itemId == R.id.navworkouts) {
+                    loadFragment(new WorkoutsFragment(),false);
+
+                } else if (itemId == R.id.navgoalsetting) {
+                    loadFragment(new GoalsFragment(),false);
+
+                } else { //nav profile
+                    loadFragment(new ProfileFragment(), false);
+                }
+
+                return true;
+            }
+        });
+
+        loadFragment(new WorkoutsFragment(), true); //load fragment when first opened app
+    }
+
+    private void loadFragment(Fragment fragment, boolean isAppInitialised) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        if (isAppInitialised){
+            fragmentTransaction.add(R.id.frameLayout, fragment);
+        } else {
+            fragmentTransaction.replace(R.id.frameLayout, fragment);
+        }
+
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
+        fragmentTransaction.commit();
+    }
+}
---
+++ app/src/main/java/com/example/digitalentrepreneurshipapp/WorkoutsFragment.java
@@ -0,0 +1,17 @@
+package com.example.digitalentrepreneurshipapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class WorkoutsFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_workouts, container, false);
+    }
+}
